d0b7bc2193f69325cf8f42f09f768784753b5738
ozanArslan2424 <136094471+ozanArslan2424@users.noreply.github.com>
2025-11-09 16:19:49 +0300

    uptime

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ const app = new Elysia()
 		},
 	})
 	.get("/", "Hello blog")
-	.get("/health", async (c) => {
+	.head("/health", async (c) => {
 		const dbHealth = await c.db.checkHealth();
 		if (!dbHealth) {
 			throw new HTTPError("DB file not found", 500);
